56026b8907b80c400493cce53bd6c9f3f6057400
liamkyoung <liamkingstonyoung@gmail.com>
2025-09-16 13:16:18 -0400

    replaced profilepic

--- src/components/Headshot.js
+++ src/components/Headshot.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import Link from 'next/link'
-import HeadshotPic from '../../public/photos/headshot.jpeg'
+import HeadshotPic from '../../public/photos/ProfilePic.png'
 import Image from 'next/image'
 
 function Headshot() {
   return (
-    <div className='flex justify-center'>
-    <Link href='/about' passHref>
-      <Image
-        height={200}
-        width={200}
-        src={HeadshotPic}
-        objectFit='contain'
-        className='rounded-full drop-shadow-2xl cursor-pointer'
-        alt='linkedin-img'
-      />
-    </Link>
-  </div>
+    <div className="flex justify-center">
+      <Link href="/about" passHref>
+        <Image
+          height={200}
+          width={200}
+          src={HeadshotPic}
+          objectFit="contain"
+          className="rounded-full drop-shadow-2xl cursor-pointer"
+          alt="linkedin-img"
+        />
+      </Link>
+    </div>
   )
 }
 
